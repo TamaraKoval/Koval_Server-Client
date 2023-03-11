3c05eac0e0889ad3fd2ff44aa928a1d79f02251c
TamaraKoval <kovalpsy@mail.ru>
2023-03-12 02:25:02 +0300

    First task done

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+
+    public static void main(String[] args) {
+
+        System.out.println("Клиент готов коннектиться!");
+
+        try (Socket socket = new Socket("localhost", 8086);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+                out.println("Однако, здравствуйте");
+                String message = in.readLine();
+                System.out.println("Сервер прислал сообщение:\n" + message);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(8086)) {
+            System.out.println("Сервер работает!=) Добро пожаловать");
+
+            while(true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    System.out.println("Установлено новое соединение\n");
+                    String message = in.readLine();
+                    System.out.println("Принятое сообщение: " + message);
+                    System.out.println("Номер порта, с которого пришло сообщение: " + clientSocket.getPort());
+
+
+                    out.println("Привет! я тебя спалил хаха! номер твоего порта: " + clientSocket.getPort());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
